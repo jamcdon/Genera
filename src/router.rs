a74be0b3afcade0e2d3fb1efe9d2bb258217d891
jared <jaredamcdon@gmail.com>
2022-04-01 01:09:36 -0500

    initial commit

---
+++ src/router.rs
@@ -0,0 +1,46 @@
+use std::net::TcpStream;
+//use std::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+use std::io;
+use std::io::Write;
+use std::thread;
+
+// adding a comment to say tokio is likely needed here or in main.rs
+// look up tokio async guide?
+// https://lib.rs/crates/tokio
+
+fn handle_client(mut stream : TcpStream) -> io::Result<()> {
+    write!(stream, "Hello, world!")?;
+    Ok(())
+}
+pub struct Server {}
+
+impl Server {
+    pub async fn connect(address: &str) -> Result<(), Box<dyn std::error::Error>> {
+        let listener = TcpListener::bind(address).await?;
+
+        loop {
+            let ( mut socket, _) = listener.accept().await?;
+
+            tokio::spawn(async move {
+                let mut buf = [0; 1024];
+
+                loop {
+                    let n = match socket.read(&mut buf).await {
+                        Ok(n) if n == 0 => return,
+                        Ok(n) => n,
+                        Err(e) => {
+                            eprintln!("Failed to read from socket; err = {:?}", e);
+                            return;
+                        }
+                    };
+                    if let Err(e) = socket.write_all(&buf[0..n]).await {
+                        eprintln!("failed to write to socket; err = {:?}", e);
+                        return;
+                    }
+                }
+            });
+        }
+    }
+}
